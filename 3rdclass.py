d4aed42c2f662370098a2c1eed92c042d0ce96fb
akkas_dev <codersadhin@gmail.com>
2024-06-13 21:54:13 +0800

    2nd

---
+++ 3rdclass.py
@@ -0,0 +1,49 @@
+# count=1
+# while (count<237):
+#     if (count%2) == 0:
+#         print(count,end=',')
+#     count+=1
+
+
+# num=int(input())
+# def defineEvenNumber(value):
+#     for i in range(1,value):
+#         if i%2==0:
+#             print(i,end=",")
+# defineEvenNumber(num)
+
+
+# prime number
+# n= int(input())
+# for i in range(1,n+1):
+#     for j in range(2,i):
+#         if i%j==0:
+#             break
+#     else:
+#         print(i,end=",")
+
+# prime number with function
+# def prime(m):
+#     for j in range(2,m):
+#         if m%j==0:
+#             return False
+#     return True
+# for i in range(1,n+1):
+#     if (prime(i)):
+#         print(i)
+
+
+# prime number with function with range
+"""
+n= int(input())
+m=int(input())
+def prime(m):
+    for j in range(2,m):
+        if m%j==0:
+            return False
+    return True
+for i in range(n,m+1):
+    if (prime(i)):
+        print(i)
+        
+"""
